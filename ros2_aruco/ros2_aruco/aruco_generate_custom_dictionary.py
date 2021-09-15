379421e1d33277d3f302ae15332e38ee77af68cb
Floris Erich <floris.erich@aist.go.jp>
2021-09-15 10:06:58 +0900

    Added a script to generate a custom marker dictionary

---
+++ ros2_aruco/ros2_aruco/aruco_generate_custom_dictionary.py
@@ -0,0 +1,39 @@
+"""
+Script for generating Aruco marker images.
+
+Author: Nathan Sprague
+Version: 10/26/2020
+"""
+
+import argparse
+import cv2
+import numpy as np
+
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                      argparse.RawDescriptionHelpFormatter):
+    """ Trick to allow both defaults and nice formatting in the help. """
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=CustomFormatter,
+                                     description="Generate a .png image of a specified maker.")
+    parser.add_argument('--size', default=200, type=int,
+                        help='Side length in pixels')
+    parser.add_argument('--num', default=100, type=int,
+                        help='Amount of markers to be generated')
+    parser.add_argument('--bits', default=6, type=int,
+                        help='Amount of bits to use')
+
+    args = parser.parse_args()
+
+    dictionary = cv2.aruco.Dictionary_create(args.num, args.bits)
+    for i in range(args.num):
+        image = np.zeros((args.size, args.size), dtype=np.uint8)
+        image = cv2.aruco.drawMarker(dictionary, i, args.size, image, 1)
+        cv2.imwrite("marker_{:04d}.png".format(i), image)
+
+
+if __name__ == "__main__":
+    main()
